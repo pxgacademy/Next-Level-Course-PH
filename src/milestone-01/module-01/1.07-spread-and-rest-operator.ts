899aecc3dd58b92f1a6c4221f4bd974aa5604e7e
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 11:11:35 +0600

    Completed 1.07-spread-and-rest-operator

---
+++ src/milestone-01/module-01/1.07-spread-and-rest-operator.ts
@@ -0,0 +1,35 @@
+{
+  // spread operator
+  // rest operator
+
+  // learn spread operator
+  const bros1: string[] = ["Rakib", "Sakib", "Akib"];
+  const bros2: string[] = ["Sadia", "Anika", "Monika"];
+  bros1.push(...bros2);
+
+  const mentors1 = {
+    typescript: "Mezba",
+    redux: "Mir",
+    dbms: "Mizan",
+  };
+
+  const mentors2 = {
+    prisma: "Firoz",
+    next: "Tonmoy",
+    cloud: "Nahid",
+  };
+
+  const mentorList = {
+    ...mentors1,
+    ...mentors2,
+  };
+
+  // learn rest operator
+  const greetFriends = (...friends: string[]) => {
+    return friends.forEach((friend: string) => {
+      `Hi ${friend}`;
+    });
+  };
+  const greetings = greetFriends("Abul", "Babul");
+  console.log(greetings);
+}
