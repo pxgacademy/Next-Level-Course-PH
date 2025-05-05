2f6ad23f989a5d6b06afad0e6c527af3bb1862d8
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 11:28:25 +0600

    Completed 2.03-introduction-to-generics

---
+++ src/milestone-01/module-02/2.03-introduction-to-generics.ts
@@ -0,0 +1,63 @@
+{
+  // generic type
+
+  type GenericArray<T> = Array<T>;
+
+  //   const rollNumber: number[] = [1, 2, 3, 4, 5];
+  //   const rollNumber: Array<number> = [1, 2, 3, 4, 5];
+  const rollNumber: GenericArray<number> = [1, 2, 3, 4, 5];
+
+  //   const mentors: string[] = ["x", "y", "z"];
+  //   const mentors: Array<string> = ["x", "y", "z"];
+  const mentors: GenericArray<string> = ["x", "y", "z"];
+
+  //   const boolArray: boolean[] = [false, true];
+  //   const boolArray: Array<boolean> = [false, true];
+  const boolArray: GenericArray<boolean> = [false, true];
+
+  //
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Abdul",
+      age: 50,
+    },
+    {
+      name: "Monika",
+      age: 35,
+    },
+  ];
+
+  //
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  const user2: GenericArray<User2> = [
+    {
+      name: "Abdul",
+      age: 50,
+    },
+    {
+      name: "Monika",
+      age: 35,
+    },
+  ];
+
+  //
+
+  // generic tuple
+
+  type GenericTuple<X, Y> = [X, Y];
+
+  const man: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
+
+  const men: GenericTuple<number, { name: string; email: string }> = [
+    1245,
+    { name: "Abul", email: "a@gmail.com" },
+  ];
+
+  //
+}
