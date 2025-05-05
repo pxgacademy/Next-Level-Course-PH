b81c8f788968014d1a6a1625e15ed2fc6519b0d9
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 14:36:47 +0600

    Completed 3.10-encapsulation-in-OOP

---
+++ dist/milestone-01/module-03/3.10-encapsulation-in-OOP.js
@@ -0,0 +1,41 @@
+"use strict";
+{
+    // encapsulation in OOP
+    class BankAccount {
+        constructor(id, name, balance, oldBalance) {
+            this.id = id;
+            this.name = name;
+            this._oldBalance = oldBalance;
+            this._balance = balance;
+        }
+        addDeposit(amount) {
+            this._oldBalance = this._balance;
+            this._balance = this._balance + amount;
+            return this._balance;
+        }
+        withdraw(amount) {
+            this._oldBalance = this._balance;
+            this._balance = this._balance - amount;
+            return this._balance;
+        }
+        oldBalance() {
+            return this._oldBalance;
+        }
+    }
+    const account1 = new BankAccount(123, "Person1", 100000, 100000);
+    account1.addDeposit(1000);
+    account1.withdraw(2000);
+    //
+    class StudentAccount extends BankAccount {
+        constructor(id, name, balance, rate) {
+            super(id, name, balance, balance);
+            this.rate = rate;
+        }
+        addFee() {
+            if (this.rate <= 0.05)
+                return this._balance * this.rate;
+            return "Enter a valid amount";
+        }
+    }
+    //
+}
