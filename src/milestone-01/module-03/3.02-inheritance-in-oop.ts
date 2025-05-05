27d32f9563f7707d7154211b8b1c1eb83c366438
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 11:51:23 +0600

    Completed 3.02-inheritance-in-oop

---
+++ src/milestone-01/module-03/3.02-inheritance-in-oop.ts
@@ -0,0 +1,52 @@
+{
+  // OOP - inheritance
+
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+
+    getSleep(numberOfHr: number) {
+      console.log(`${this.name} sleeps for ${numberOfHr}`);
+    }
+  }
+
+  //
+
+  class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
+
+  //
+
+  class Teacher extends Parent {
+    designation: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+
+    takeClass(numberOfClass: number) {
+      console.log(`${this.name} takes ${numberOfClass} class(s)`);
+    }
+  }
+
+  const student1 = new Student("Monika", 25, "Ctg");
+  const teacher1 = new Teacher("Abul", 50, "Ctg", "General Teacher");
+
+  //
+}
