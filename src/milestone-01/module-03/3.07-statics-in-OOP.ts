8b462f4065ef9545c77dcee408dbc8162b3cd552
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 12:36:23 +0600

    Completed 3.07-statics-in-OOP

---
+++ src/milestone-01/module-03/3.07-statics-in-OOP.ts
@@ -0,0 +1,25 @@
+{
+  // Static in OOP
+
+  class Counter {
+    static count: number = 0;
+
+    increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+
+    static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
+
+  const instance1 = new Counter();
+  console.log(instance1.increment());
+
+  const instance2 = new Counter();
+  console.log(instance2.increment());
+
+  console.log(Counter.decrement());
+
+  //
+}
