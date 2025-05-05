5b2aebd6c3f2320db85c0ef7d53f44a39fe28592
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 12:17:42 +0600

    Completed 3.06-getter-and-setter

---
+++ src/milestone-01/module-03/3.06-getter-and-setter.ts
@@ -0,0 +1,50 @@
+{
+  // getter and setter
+
+  class BankAccount {
+    public readonly id: number; // only can read access from anywhere
+    public name: string; // can access from anywhere
+    private _oldBalance: number; // only can access into this class
+    protected _balance: number; // only can access into this class and child class
+
+    constructor(id: number, name: string, balance: number, oldBalance: number) {
+      this.id = id;
+      this.name = name;
+      this._oldBalance = oldBalance;
+      this._balance = balance;
+    }
+
+    // addDeposit(amount: number): number {
+    //   this._oldBalance = this._balance;
+    //   this._balance = this._balance + amount;
+    //   return this._balance;
+    // }
+
+    set addDeposit(amount: number) {
+      this._oldBalance = this._balance;
+      this._balance = this._balance + amount;
+    }
+
+    withdraw(amount: number): number {
+      this._oldBalance = this._balance;
+      this._balance = this._balance - amount;
+      return this._balance;
+    }
+
+    //   oldBalance(): number {
+    //     return this._oldBalance;
+    //   }
+
+    // getter
+
+    get oldBalance() {
+      return this._oldBalance;
+    }
+  }
+
+  const account1 = new BankAccount(123, "Person1", 100000, 100000);
+  account1.addDeposit = 5000;
+  console.log(account1.oldBalance);
+
+  //
+}
---
+++ zustand_clone.js
@@ -0,0 +1,41 @@
+// zustandClone.js
+import { useState, useEffect } from "react";
+
+export function createStore(storeCreator) {
+  let state = storeCreator(set, get); // initial state
+  const listeners = new Set(); // subscribed components
+
+  function set(partial) {
+    const nextState = typeof partial === "function" ? partial(state) : partial;
+    state = { ...state, ...nextState };
+
+    // সব subscribed component কে inform করো
+    listeners.forEach((listener) => listener());
+  }
+
+  function get() {
+    return state;
+  }
+
+  function useStore(selector = (s) => s) {
+    const [selectedState, setSelectedState] = useState(() =>
+      selector(state)
+    );
+
+    useEffect(() => {
+      function handleChange() {
+        const newState = selector(state);
+        setSelectedState(newState);
+      }
+
+      listeners.add(handleChange);
+
+      // Cleanup
+      return () => listeners.delete(handleChange);
+    }, []);
+
+    return selectedState;
+  }
+
+  return useStore;
+}
