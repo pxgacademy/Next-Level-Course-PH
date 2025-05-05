d649bffceacc2293e4d8a598519b567bee73816e
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 11:46:10 +0600

    Completed 2.10-mapped-types

---
+++ src/milestone-01/module-02/2.10-mapped-types.ts
@@ -0,0 +1,46 @@
+{
+  // mapped types
+
+  const arrayOfNumbers: number[] = [1, 2, 3, 4, 5];
+
+  //   const arrayOfString: string[] = ["1", "2", "3", "4"];
+  const arrayOfString: string[] = arrayOfNumbers.map((num) => num.toString());
+
+  //
+
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+
+  //   type typeString = {
+  //     height: string
+  //     width: string
+  //   }
+
+  //   type AreaString = {
+  //     [index in "height" | "width"]: string;
+  //   };
+
+  type Height = AreaNumber["height"]; // look up type
+
+  //   type AreaString = {
+  //     [index in keyof AreaNumber]: string;
+  //   };
+
+  //
+  //
+
+  // T => { height: string; width: number }
+  // T[index] => T['height'] / T[index] => T['width']
+  type AreaString<T> = {
+    [index in keyof T]: T[index]; // look up type
+  };
+
+  const area1: AreaString<{ height: string; width: number }> = {
+    height: "100",
+    width: 50,
+  };
+
+  //
+}
