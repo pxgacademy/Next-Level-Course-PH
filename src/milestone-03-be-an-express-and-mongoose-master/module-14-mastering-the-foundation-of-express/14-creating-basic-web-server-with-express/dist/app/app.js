7c28ebc3a048031ccdc06346481021dfbd8341d0
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-23 14:43:50 +0600

    updated

--- src/milestone-03-be-an-express-and-mongoose-master/module-14-mastering-the-foundation-of-express/14-creating-basic-web-server-with-express/dist/app/app.js
+++ src/milestone-03-be-an-express-and-mongoose-master/module-14-mastering-the-foundation-of-express/14-creating-basic-web-server-with-express/dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,7 +19,21 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/todos", route_todos_1.default);
 app.use("/users", user_todos_1.default);
-app.get("/", (req, res) => {
-    res.send("I am working from module 14.02");
+app.get("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send("I am working from module 14.02");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+app.use((error, req, res, next) => {
+    if (error)
+        res
+            .status(500)
+            .json({ message: "Something went wrong", error: error.message });
 });
 exports.default = app;
--- src/milestone-03-be-an-express-and-mongoose-master/module-14-mastering-the-foundation-of-express/14-creating-basic-web-server-with-express/src/app/app.ts
+++ src/milestone-03-be-an-express-and-mongoose-master/module-14-mastering-the-foundation-of-express/14-creating-basic-web-server-with-express/src/app/app.ts
@@ -1,4 +1,10 @@
-import express, { Application, Request, Response, Router } from "express";
+import express, {
+  Application,
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import todoRouter from "./todos/route.todos";
 import userRouter from "./todos/user.todos";
 const app: Application = express();
@@ -7,8 +13,23 @@ app.use(express.json());
 app.use("/todos", todoRouter);
 app.use("/users", userRouter);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("I am working from module 14.02");
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send("I am working from module 14.02");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error)
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: error.message });
 });
 
 export default app;
